9e23d3ed7db8f93e3f02cbdb35ea7f8151592685
Suman <stha.suman327@gmail.com>
2022-06-22 12:58:35 +0545

    chapter 8

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,22 +6,37 @@ export class AppController {
   @Get("/")
   @Render('index')
   index(){
-    return {
-      title: "Home Page - Online Store"
+    // return {
+    //   title: "Home Page - Online Store"
+    // };
+
+    let viewData = [];
+    viewData['title'] = 'Home Page - Online Store';
+    return{
+      viewData: viewData
     };
   }
 
   @Get("/about")
   @Render('about')
   about(){
-    let viewData = [];
-    viewData["description"] = "This is an about page...";
-    viewData['author'] = "Developed By: AAA";
-    let data1 = 'About us - Online Store';
+    // let viewData = [];
+    // viewData["description"] = "This is an about page...";
+    // viewData['author'] = "Developed By: AAA";
+    // let data1 = 'About us - Online Store';
+    // return {
+    //   title: data1,
+    //   subtitle: "About us",
+    //   viewData: viewData
+    // };
+
+    const viewData = [];
+    viewData['title'] = 'About-us Online Store';
+    viewData['sibtitle'] = 'About Us';
+    viewData['description'] = "This is an about page";
+    viewData['author'] = 'Developed By: AAAA';
     return {
-      title: data1,
-      subtitle: "About us",
-      viewData: viewData
+      viewData:viewData,
     };
   }
  
