b543be61f89f382757ec2dbcf102338f1830525c
Suman <stha.suman327@gmail.com>
2022-06-22 14:44:16 +0545

    chapter 13

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { Product } from './models/product.entity';
 import { ProductsController } from './product.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "online_store",
+      entities: [Product],
+      synchronize: true
+    }),
+  ],
   controllers: [AppController, ProductsController],
 })
 export class AppModule {}
